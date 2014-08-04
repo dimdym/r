bc91397e77f62c837cfe6180e7c4efe8e148543a
dimdym <dimdym@gmail.com>
2014-08-04 15:52:58 -0500

    script for displaying multiple learning curves

---
+++ lc.r
@@ -0,0 +1,26 @@
+data <- read.table('t2d.txt')
+
+ymin <- min(data$V2, data$V4, data$V6, data$V8)
+ymax <- max(data$V2, data$V4, data$V6, data$V8)
+
+# yaxt: suppress y axis (to be created manually below)
+# type: line vs. dots etc.
+plot(data$V1, 
+     data$V2,
+     xlab="Training set size",
+     ylab="Classification accuracy",
+     ylim=c(ymin, ymax),
+     yaxt="n", 
+     type="l", 
+     col="blue")
+
+# label orientation for y axis
+axis(2, las=2) 
+
+lines(data$V1, data$V4, col="green")
+lines(data$V1, data$V6, col="cyan")
+lines(data$V1, data$V8, col="purple")
+
+legend("bottomright", 
+       c("labeled only", "500", "1000", "3000"),
+       fill=c("blue", "green", "cyan", "purple"))
