d9c419d887dc480792ca83b49f7dc2f72c0aa8f1
dimdym <dimdym@gmail.com>
2014-09-18 12:51:31 -0500

    added code to compute AUCs

--- all-curves.r
+++ all-curves.r
@@ -2,13 +2,20 @@
 make.plots <- function() {
 
   phenotypes <- c("cd", "uc", "ms", "t2d")
-  
+
+  cat(sprintf("%3s %5s %5s %5s\n", "", "500", "1000", "3000"))
   for(phenotype in phenotypes) {
     make.plot(phenotype)
+    auc.improvement(phenotype)
   }
 }
 
 # generate a plot for a single phenotype (e.g. "cd")
+# column1: training set sizes
+# column2: supervised performance (0 unlabeled examples)
+# column4: performance with 500 unlabeled examples
+# column6: performance with 1000 unlabeled examples
+# column8: performance with 5000 unlabeled examples
 make.plot <- function(phenotype) {
 
   base <- "/Users/Dima/Boston/Out/"
@@ -41,6 +48,29 @@ make.plot <- function(phenotype) {
          fill=c("blue", "green", "cyan", "purple"))
 
   dev.off()
+}
+
+# calcuate semi-supervised curve auc minus baseline auc
+auc.improvement <- function(phenotype) {
+
+  require(pracma, quietly=TRUE)
+  
+  base <- "/Users/Dima/Boston/Out/"
+  file <- paste(base, phenotype, ".txt", sep="")
+  out <- paste(base, phenotype, ".pdf", sep="")
+  data <- read.table(file)
+
+  auc0 <- trapz(data$V1, data$V2)
+  auc500 <- trapz(data$V1, data$V4)
+  auc1000 <- trapz(data$V1, data$V6)
+  auc3000 <- trapz(data$V1, data$V8)
+
+  dif500 <- auc500 - auc0
+  dif1000 <- auc1000 - auc0
+  dif3000 <- auc3000 - auc0
+  
+  out <- sprintf("%3s %.3f %.3f %.3f\n", phenotype, dif500, dif1000, dif3000)
+  cat(out)
 }
 
 # run make.plots function
