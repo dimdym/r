0ac72f8e9225f4060ff99632e459451226567a37
dimdym <dimdym@gmail.com>
2014-09-29 10:11:09 -0500

    improved formatting

--- aucs.r
+++ aucs.r
@@ -2,7 +2,7 @@
 all.auc <- function() {
 
   phenotypes <- c("cd", "uc", "ms", "t2d")
-  cat(sprintf("%5s %6s %6s %6s\n", "", "500", "1000", "3000"))
+  cat(sprintf("%3s %7s %7s %7s\n", "", "500", "1000", "3000"))
 
   total_auc <- 0
   for(phenotype in phenotypes) {
@@ -10,7 +10,7 @@ all.auc <- function() {
     total_auc <- total_auc + phenotype_auc
   }
 
-  cat(sprintf("%5s %7.3f\n", "all", total_auc))
+  cat(sprintf("%3s %7.3f\n", "av", total_auc / 12))
 }
 
 # calcuate semi-supervised curve auc minus baseline auc
@@ -37,7 +37,7 @@ auc.improvement <- function(phenotype) {
   dif1000 <- auc1000 - auc0
   dif3000 <- auc3000 - auc0
   
-  out <- sprintf("%5s %6.3f %6.3f %6.3f\n", phenotype, dif500, dif1000, dif3000)
+  out <- sprintf("%3s %7.3f %7.3f %7.3f\n", phenotype, dif500, dif1000, dif3000)
   cat(out)
 
   return(dif500 + dif1000 + dif3000)
