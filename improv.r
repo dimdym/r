13fd1937c10a33cca170e2f8e3c6e5daa3866b87
dimdym <dimdym@gmail.com>
2014-10-24 13:34:51 -0500

    removed unused function

--- improv.r
+++ improv.r
@@ -33,23 +33,6 @@ average.improvement <- function(directory, phenotype) {
   return(mean(diff500) + mean(diff1000) + mean(diff3000))
 }
 
-# calculate percent of points above baseline curve
-above.baseline <- function(phenotype) {
-
-  data <- load.results(phenotype)
-
-  dif500 <- data$u500 - data$u0
-  dif1000 <- data$u1000 - data$u0
-  dif3000 <- data$u3000 - data$u0
-
-  rat500 <- length(dif500[dif500 > 0]) / length(data$u0)
-  rat1000 <- length(dif1000[dif1000 > 0]) / length(data$u0)
-  rat3000 <- length(dif3000[dif3000 > 0]) / length(data$u0)
-
-  out <- sprintf("%3s %.3f %.3f %.3f\n", phenotype, rat500, rat1000, rat3000)
-  cat(out)
-}
-
 # main method
 source("common.r")
 for(experiment.directory in list.files(RESULTROOT)) {
